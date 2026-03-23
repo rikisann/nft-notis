51c803776fcf588249f2abd47ce31936a0f29db9
rikisann <rikisann@users.noreply.github.com>
2026-03-23 20:11:05 +0000

    move hardcoded credentials to env vars

--- dist/lib/sendMessage.js
+++ dist/lib/sendMessage.js
@@ -2,34 +2,18 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: "REDACTED_EMAIL",
-        pass: "REDACTED_APP_PASSWORD",
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
     },
 });
 export default async function sendMessage(message) {
     console.log("Sending message...");
     const mailOptions = {
-        from: "Nft Notis <REDACTED_EMAIL>",
-        to: "REDACTED_PHONE_GATEWAY",
+        from: `Nft Notis <${process.env.SMTP_USER}>`,
+        to: process.env.NOTIFY_EMAIL,
         subject: message.subject,
         text: message.text,
     };
     await transporter.sendMail(mailOptions);
     console.log("Sent through!");
 }
-// console.log("Sending message...");
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 587,
-//   auth: {
-//     user: "REDACTED_EMAIL",
-//     pass: "REDACTED_APP_PASSWORD_2",
-//   },
-// });
-// const response = transporter.sendMail({
-//   from: '"nft noti" <REDACTED_EMAIL>',
-//   to: "REDACTED_PHONE_GATEWAY_2",
-//   subject: message.subject,
-//   text: message.text,
-// });
-// console.log("Sent message");
--- src/lib/sendMessage.ts
+++ src/lib/sendMessage.ts
@@ -8,16 +8,16 @@ export type Message = {
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "REDACTED_EMAIL",
-    pass: "REDACTED_APP_PASSWORD",
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
 export default async function sendMessage(message: Message) {
   console.log("Sending message...");
   const mailOptions = {
-    from: "Nft Notis <REDACTED_EMAIL>",
-    to: "REDACTED_PHONE_GATEWAY",
+    from: `Nft Notis <${process.env.SMTP_USER}>`,
+    to: process.env.NOTIFY_EMAIL,
     subject: message.subject,
     text: message.text,
   };
@@ -25,22 +25,3 @@ export default async function sendMessage(message: Message) {
   await transporter.sendMail(mailOptions);
   console.log("Sent through!");
 }
-
-// console.log("Sending message...");
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 587,
-//   auth: {
-//     user: "REDACTED_EMAIL",
-//     pass: "REDACTED_APP_PASSWORD_2",
-//   },
-// });
-
-// const response = transporter.sendMail({
-//   from: '"nft noti" <REDACTED_EMAIL>',
-//   to: "REDACTED_PHONE_GATEWAY_2",
-//   subject: message.subject,
-//   text: message.text,
-// });
-
-// console.log("Sent message");
